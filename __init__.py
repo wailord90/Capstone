8e291374e75d0ecefc81ff6a0876b44fda7bfcd3
Caleb Jobe <cjobe@fenwaygroup.com>
2019-02-07 02:38:56 -0600

    added db test

--- __init__.py
+++ __init__.py
@@ -7,15 +7,28 @@ import json
 import pprint
 import pusher 
 from datetime import datetime
-
+from flask_sqlalchemy import SQLAlchemy
 # load_dotenv('.myenv')
 # pprint.pprint(dict(os.environ))
 
+
+
+
+
 app = Flask(__name__)
+db = SQLAlchemy(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://user:root@localhost/secure_server_db'
 Bootstrap(app)
+
+
 @app.route('/')
 def index():
   return render_template('index.html')
+
+class Example(db.Model):
+	__tablename__ = 'example'
+	id = db.Column('id', db.Integer, primary_key=True)
+	data = db.Column('data', db.Unicode)
 
 if __name__ == '__main__':
     app.run(debug=True)
