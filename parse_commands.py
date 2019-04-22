f083270ecc833d2fd0c569124553a8bcf1d5fd3c
Caleb Jobe <cjobe@fenwaygroup.com>
2019-04-22 17:58:30 -0500

    changes

--- parse_commands.py
+++ parse_commands.py
@@ -1,21 +1,18 @@
-import subprocess
+import os
 import re
-
-
+import sys
 
 def main(argv):
     print("yup")
-    
-
-
-
+    os.system("ausearch -ua root -i | grep cmd -B 3 > tmpfile.txt")
+    file = open("tmpfile.txt", "r")
+    contents = f.read()
+    print contents
 
 
 def parse():
     re.search
 
 
-
-
-if __name__ == '__main__';
+if __name__ == '__main__':
     main(sys.argv[1:])
