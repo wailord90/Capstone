1efcd835d2d0ea3d3d9206f97c1d0dc83a82a4ba
root <cjobe@fenwaygroup.com>
2019-04-23 22:42:57 +0000

    added regex

--- parse_commands.py
+++ parse_commands.py
@@ -14,18 +14,42 @@ def main(argv):
                 lines = []
         if len(lines) > 0:
             parse(lines)
-    print contents
 
 
 def parse(lines):
-    cmd = re.search("(?<=\scmd=)[a-z]+",lines)
+    lines = str(lines)
+    print lines
+    cmd = re.search(r'(?<=\scmd=)[a-z]+',lines)
+    if cmd:
+        cmd = cmd.group(0)
+    else:
+        cmd ="none"
     uid = re.search("(?<=\suid=)[a-z]+",lines)
+    if uid:
+        uid= uid.group(0)
+    else:
+        uid="none"
     auid = re.search("(?<=\sauid=)[a-z]+",lines)
+    if auid:
+        auid= auid.group(0)
+    else:
+        auid= "none"
     time = re.search("([0-9]+\/[0-9]+\/[0-9]+ [0-9]+:[0-9]+:[0-9]+.[0-9]+:[0-9]+)",lines)
+    if time:
+        print time
+        time=time.group(0)
+    else:
+        time ="none"
     proctitle = re.search("(?<=proctitle=)[a-z]+",lines)
-    print auid +" "+ uid +" "+ time  +" "+ cmd +" "+ proctitle
-
-
+    if proctitle:
+        proctitle = proctitle.group(0)
+    else:
+        proctitle = "none"
+    print auid +" "
+    print uid +" "
+    print time  +" "
+    print cmd +" "
+    print proctitle
 
 if __name__ == '__main__':
     main(sys.argv[1:])
