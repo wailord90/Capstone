2a029782dedaf46a65f35a38360bdc2cc94d4529
Caleb Jobe <cjobe@fenwaygroup.com>
2019-04-22 19:50:31 -0500

    added some parsing

--- parse_commands.py
+++ parse_commands.py
@@ -5,13 +5,26 @@ import sys
 def main(argv):
     print("yup")
     os.system("ausearch -ua root -i | grep cmd -B 3 > tmpfile.txt")
-    file = open("tmpfile.txt", "r")
-    contents = f.read()
+    with open('tmpfile.txt', 'r') as infile:
+        lines = []
+        for line in infile:
+            lines.append(line)
+            if len(lines) >= 3:
+                parse(lines)
+                lines = []
+        if len(lines) > 0:
+            parse(lines)
     print contents
 
 
-def parse():
-    re.search
+def parse(lines):
+    cmd = re.search("(?<=\scmd=)[a-z]+",lines)
+    uid = re.search("(?<=\suid=)[a-z]+",lines)
+    auid = re.search("(?<=\sauid=)[a-z]+",lines)
+    time = re.search("([0-9]+\/[0-9]+\/[0-9]+ [0-9]+:[0-9]+:[0-9]+.[0-9]+:[0-9]+)",lines)
+    proctitle = re.search("(?<=proctitle=)[a-z]+",lines)
+    print auid +" "+ uid +" "+ time  +" "+ cmd +" "+ proctitle
+
 
 
 if __name__ == '__main__':
