4efe9427931acdf02c6314ba637cc160ce1ddb53
Caleb Jobe <cjobe@fenwaygroup.com>
2019-04-25 08:34:31 -0500

    fixed format

--- db_orch.py
+++ db_orch.py
@@ -9,7 +9,7 @@ def create_db():
 def add_session(time, uid, auid, cwd, pid, a2):
     datetime_object = datetime.strptime(time, '%m/%d/%Y %H:%M:%S.%f')
     user_sess = User_Sessions(time=datetime_object,
-                              uid=username, auid=auid, cwd=cwd, pid=pid, a2=a2)
+                              uid=uid, auid=auid, cwd=cwd, pid=pid, a2=a2)
     db.session.add(user_sess)
     db.session.commit()
 
@@ -19,4 +19,4 @@ def query_sessions():
 
 
 def query_filtered_sessions(fil):
-    return User_Sessions.query.filter_by(username=fil).first()
+    return User_Sessions.query.filter_by(uid=fil).first()
