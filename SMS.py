9d0ad5737883d216506d85919a73cd23ace67c02
daj024 <daj024@latech.edu>
2019-05-02 14:43:23 -0500

    Cleaned up code

--- SMS.py
+++ SMS.py
@@ -1,13 +1,9 @@
-# Download the helper library from https://www.twilio.com/docs/python/install
 import os
 
 from twilio.rest import Client
 
 
 def sendText(body, to):
-
-    # Your Account Sid and Auth Token from twilio.com/console
-    # DANGER! This is insecure. See http://twil.io/secure
 
     account_sid = os.environ['TWILIO_ACCOUNT_SID']
     auth_token = os.environ['TWILIO_AUTH_TOKEN']
